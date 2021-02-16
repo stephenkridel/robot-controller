b31b47990a02938a6dd5b98310a9da2795f232a8
Stephen Kridel <stephen.kridel@gmail.com>
2021-02-16 10:47:58 -0500

    Deleted some comments at the top that were left from the Sweep example code I worte all of this overtop of

--- RobotController.ino
+++ RobotController.ino
@@ -1,11 +1,3 @@
-/* Sweep
-  by BARRAGAN <http://barraganstudio.com>
-  This example code is in the public domain.
-
-  modified 8 Nov 2013
-  by Scott Fitzgerald
-  http://www.arduino.cc/en/Tutorial/Sweep
-*/
 
 #include <Servo.h>
 // #include <Base64.h>
